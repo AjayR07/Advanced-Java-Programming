646fefbf854b5b7b5c9297d3efd105acd3043d38
AjayR07 <ajayr.18cse@kongu.edu>
2020-07-19 16:26:15 +0530

    Init Commit for JFrame Creation

---
+++ Java Swing Basics/src/JFrameCreation/CreateJFrame.java
@@ -0,0 +1,15 @@
+package JFrameCreation;
+
+import javax.swing.*;
+public class CreateJFrame {
+
+	public static void main(String[] args) {
+		JFrame f=new JFrame();
+		f.setTitle("My Frame");
+		f.setSize(300,400);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
